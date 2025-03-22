d42fbe87c03104ef862a19eec4a1348b39c25d60
Eda Belge <edabelge00@gmail.com>
2025-03-22 23:05:30 +0300

    chore: re-arrange projects structres

--- CustomerOrders.Core/Interfaces/IRepository.cs
+++ CustomerOrders.Core/Interfaces/IRepository.cs
@@ -1,6 +1,6 @@
 ﻿using System.Linq.Expressions;
 
-namespace CustomerOrders.Application.Interfaces
+namespace CustomerOrders.Core.Interfaces
 {
     public interface IRepository<T> where T : class
     {
