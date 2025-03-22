9fad93a0ca3f577ebed99ee9845633006ea74aac
Eda Belge <edabelge00@gmail.com>
2025-03-22 23:08:22 +0300

    chore: add product related requests

---
+++ CustomerOrders.Application/DTOs/RequestDtos/CreateProductRequest.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// Holds the essential product data required to create a new product.
+/// </summary>
+public class CreateProductRequest
+{
+    public string Barcode { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
